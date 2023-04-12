714abd6e808d3bbf906feab29c8c3eb1fbedfc2b
syedma <ahrissyed@gmail.com>
2023-04-12 16:11:53 +0500

    fixed code to use MockRepository

--- DAL/Data/Data.py
+++ DAL/Data/Data.py
@@ -1,9 +1,9 @@
-from DAL.Repository.Repository import Repository
+from DAL.Repository.MockRepository import MockRepository
 
 
 class Data:
     def __init__(self):
-        self.repository = Repository()
+        self.repository = MockRepository()
         self.stream_links = self.repository.get_stream_links()
         self.stream_threads = []
         self.stream_frames = {}
