92cf4c0c711584ad53b9ef1993d3649cf87b77e8
syedma <ahrissyed@gmail.com>
2023-04-12 15:28:04 +0500

    removed unused import

--- DAL/Model/Intersection_Model.py
+++ DAL/Model/Intersection_Model.py
@@ -1,6 +1,8 @@
+from DAL.Model.Signal_Model import SignalModel
+
+
 class IntersectionModel:
     def __init__(self, _id: str, signals: list):
         self._id = _id
         self.signals = signals
-
 
