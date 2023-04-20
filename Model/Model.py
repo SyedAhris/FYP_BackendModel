398233212ee200f0b6ad44abd35e3e86068d0e03
IrtizaR <irtizaraza09@gmail.com>
2023-04-20 15:28:58 +0500

    basc struc complete

--- Model/Model.py
+++ Model/Model.py
@@ -18,9 +18,10 @@ class Model:
         count = pred.pandas().xyxy[0].shape[0]
         # TODO calculate count for emergency and non emergency both
 
-        print(res)
+        # print(res)
         # Output:
         # [{"xmin":365.4317321777,"ymin":170.4187011719,"xmax":375.5906066895,"ymax":178.2403564453,"confidence":0.2531405985,"class":1,"name":"car"}]
+
         for bbox in pred.xyxy[0]:
             xmin, ymin, xmax, ymax = bbox[:4]
             cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 255, 0), 4)
@@ -28,27 +29,3 @@ class Model:
         return [[count, 0], frame]  # returns unnanoted frame for now for checking purposes and [count, 0] for testing purposes
 
         # TODO@irtiza:annotate the frame and return it instead of the original frame
-
-        # annotator = Annotator(img, line_width=3, example=str(self.model.names))
-        # for i, det in enumerate(pred):
-        #     if len(det):
-        #         for *xyxy, conf, cls in reversed(det):
-        #             c = int(cls)  # integer class
-        #             label = None if False else (self.model.names[c] if False else f'{self.model.names[c]} {conf:.2f}')
-        #             annotator.box_label(xyxy, label, color=colors(c, True))
-        #
-        # img = annotator.result()
-        # return the frame
-        # return img
-
-        # pred = non_max_suppression(pred, conf_thres=0.5, iou_thres=0.5)[0]
-        #
-        # # Draw boxes on the original frame
-        # if pred is not None:
-        #     pred[:, :4] = scale_coords(img.shape[2:], pred[:, :4], frame.shape).round()
-        #
-        #     for *xyxy, conf, cls in pred:
-        #         label = f'{self.model.names[int(cls)]} {conf:.2f}'
-        #         plot_one_box(xyxy, frame, label=label, color=(0, 255, 0), line_thickness=3)
-        #
-        # cv2.imshow(img)
